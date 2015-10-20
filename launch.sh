c9b785ee46122a246e75100f4d5ef479dbdb9654
Mahak-Patil <MahakPatil@dhcp137.merusouth115.iit.edu>
2015-10-20 12:43:35 -0500

    updated script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id  $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 
+aws ec2 run-instances --image-id  $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6 -iam-profile $7
+
 
 
