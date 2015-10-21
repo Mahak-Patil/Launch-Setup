e860778b09cd237f4295ec67bae7d5ebf1408d1f
Mahak-Patil <MahakPatil@dhcp166.merusouth118.iit.edu>
2015-10-20 20:47:05 -0500

    updated comments

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#"general" launch script. will accept 7 arguments when running script.
 aws ec2 run-instances --image-id  $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6 --iam-profile $7  --associate-public-ip-address
