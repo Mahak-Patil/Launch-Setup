27a4f845ee54f5502158cd263097952b8e96cbc5
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-06 20:09:27 -0600

    updates to install-webserver.sh

---
+++ install-webserver.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git mysql-client php5 php5-curl curl php5-mysql
+
+git clone https://github.com/Mahak-Patil/Launch-Setup
+git clone https://github.com/Mahak-Patil/Environment-Setup
+git clone https://github.com/Mahak-Patil/Application-Setup 
+
+mv ./Environment-Setup/images /var/www/html/images
+
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
+#cleanup script starts here:
+
+declare -a cleanupARR 
+declare -a cleanupLBARR
+declare -a dbInstanceARR
+
+aws ec2 describe-instances --filter Name=instance-state-code,Values=16 --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g"
+
+mapfile -t cleanupARR < <(aws ec2 describe-instances --filter Name=instance-state-code,Values=16 --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+
+echo "the output is ${cleanupARR[@]}"
+
+aws ec2 terminate-instances --instance-ids ${cleanupARR[@]} 
+
+echo "Cleaning up existing Load Balancers"
+mapfile -t cleanupLBARR < <(aws elb describe-load-balancers --output json | grep LoadBalancerName | sed "s/[\"\:\, ]//g" | sed "s/LoadBalancerName//g")
+
+echo "The LBs are ${cleanupLBARR[@]}"
+
+LENGTH=${#cleanupLBARR[@]}
+echo "ARRAY LENGTH IS $LENGTH"
+for (( i=0; i<${LENGTH}; i++)); 
+  do
+  aws elb delete-load-balancer --load-balancer-name ${cleanupLBARR[i]} --output text
+  sleep 1
+done
+
+# Delete existing RDS  Databases
+# Note if deleting a read replica this is not your command 
+mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
+
+if [ ${#dbInstanceARR[@]} -gt 0 ]
+   then
+   echo "Deleting existing RDS database-instances"
+   LENGTH=${#dbInstanceARR[@]}  
+
+   # http://docs.aws.amazon.com/cli/latest/reference/rds/wait/db-instance-deleted.html
+      for (( i=0; i<${LENGTH}; i++));
+      do 
+      aws rds delete-db-instance --db-instance-identifier ${dbInstanceARR[i]} --skip-final-snapshot --output text
+      aws rds wait db-instance-deleted --db-instance-identifier ${dbInstanceARR[i]} --output text
+      sleep 1
+   done
+fi
+
+# Create Launchconf and Autoscaling groups
+
+LAUNCHCONF=(`aws autoscaling describe-launch-configurations --output json | grep LaunchConfigurationName | sed "s/[\"\:\, ]//g" | sed "s/LaunchConfigurationName//g"`)
+
+SCALENAME=(`aws autoscaling describe-auto-scaling-groups --output json | grep AutoScalingGroupName | sed "s/[\"\:\, ]//g" | sed "s/AutoScalingGroupName//g"`)
+
+echo "The asgs are: " ${SCALENAME[@]}
+echo "the number is: " ${#SCALENAME[@]}
+
+if [ ${#SCALENAME[@]} -gt 0 ]
+  then
+echo "SCALING GROUPS to delete..."
+#aws autoscaling detach-launch-
+
+#aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME
+
+#aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
+
+#aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --min-size 0 --max-size 0
+
+#aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME
+#aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
+fi
+
+echo "All done"
+
