cce817cc61c10acb99fd5b9381f0a733d0768140
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 19:15:31 -0600

    making db changes in gallery.php

--- gallery.php
+++ gallery.php
@@ -1,5 +1,10 @@
 <html>
 <head><title>Gallery</title>
+ <!--jQuery-->
+  <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <!--Fotorama-->
+  <link href="fotorama.css" rel="stylesheet">
+  <script src="fotorama.js"></script>
 </head>
 <body>
 
