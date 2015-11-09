929a41b4cee861d65e35cf03be449f0c10785424
Mahak-Patil <MahakPatil@dhcp251.merunorth107.iit.edu>
2015-11-08 19:16:10 -0600

    last few touches

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 # This script launches: database subnet, AWS RDS instances, EC2 instances,read replica of created database, load balancer, cloud metrics and autoscaling group.
 # This script needs 7 arguments: ami image-id, number of EC2 instances, instance type, security group ids, subnet id, key name and iam profile
 
-
+echo "Initiating cleaup. Please be patient and wait for the next prompt"
 #cleanup script provided by Jeremy Hajek starts here:
 
 declare -a cleanupARR 
@@ -72,12 +72,82 @@ echo "SCALING GROUPS to delete..."
 #aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME
 #aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
 fi
+echo "All done! Thank you for your patience"
 
-echo "All done"
+echo "\nEnter arguments in the following order: ami image id (e.g ami-d05e75b8), number of EC2 instances needed (e.g 3), instance type (e.g.t2.micro), security group ID (e.g. sg- ), subnet ID(e.g. subnet- ), key pair name (make sure the path is correct) and IAM Profile: "
+
+# creating database subnet
+DbSubnetID=$(aws rds create-db-subnet-group --db-subnet-group-name ITMO-544-Database-Subnet --subnet-ids $SubnetID1 $SubnetID2 --db-subnet-group-description "Database subnet" --output=text)
+echo "\nDatabase subnet created: "$DbSubnetID
+# creating the database
+aws rds create-db-instance --db-instance-identifier ITMO-544-Database --allocated-storage 5 --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password ilovebunnies --db-subnet-group-name ITMO-544-Database-Subnet --db-name ITMO-544-Database 
+cho "Sleeping for one minute"
+for i in {0..60}
+do
+echo -ne '.'
+sleep 1
+done
+
+# creating elb 
+ElbUrl=$(aws elb create-load-balancer --load-balancer-name ITMO-544-Load-Balancer --security-groups sg-414a0a26 --subnets $SubnetID1 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --output=text)
+echo "\nLaunched ELB and sleeping for one minute"
+for i in {0..60}
+ do
+  echo -ne '.'
+  sleep 1;
+  done
+
+# configuring health check
+aws elb configure-health-check --load-balancer-name ITMO-544-Load-Balancer --health-check Target=HTTP:80/index.php,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+echo -e "\nConfigured ELB health check. Proceeding to launch EC2 instances"
+  
+# launching ec2 instances
+aws ec2 run-instances --image-id ami-d05e75b8 --count 3 --instance-type t2.micro --key-name ITMO-544-Key-Pair --user-data file://install-webserver.sh --subnet-id subnet-0fdfdd78 --output text --security-group-ids sg-414a0a26 --iam-instance-profile Name=phpDeveloper
+echo -e "\nLaunched 3 EC2 Instances and sleeping for one minute"
+for i in {0..60}
+ do
+  echo -ne '.'
+  sleep 1;
+  done
+  
+ 
+# registering instances with crested elb
+declare -a instance_list
+mapfile -t instance_list < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+aws ec2 wait instance-running --instance-ids ${instance_list[@]} 
+aws ec2 wait instance-running --instance-ids ${instance_list[@]} 
+echo "Following instances running: ${instance_list[@]}" 
+echo "\nAdding above to an array and registering with the load balancer." 
+len=${#instance_list[@]}
+for (( i=0; i<${#instance_list[@]}; i++)); 
+  do
+  echo "Registering ${instance_list[$i]} with load-balancer ITMO-544-Load-Balancer" 
+  aws elb register-instances-with-load-balancer --load-balancer-name ITMO-544-Load-Balancer --instances ${instance_list[$i]} --output=table 
+echo -e "\n Sleeping for one minute to complete the process."
+    for y in {0..60} 
+    do
+      echo -ne '.'
+      sleep 1
+    done
+ echo "\n"
+done
 
 
 
-echo "Enter arguments in the following order: ami image id (e.g ami-d05e75b8), number of EC2 instances needed (e.g 3), instance type (e.g.t2.micro), security group ID (e.g. sg- ), subnet ID(e.g. subnet- ), key pair name (make sure the path is correct) and IAM Profile: "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # launch database here:
