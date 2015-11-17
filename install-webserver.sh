7f98054dbe782d9bad97244ed664668d94f9e48f
Mahak-Patil <MahakPatil@dhcp97.merunorth99.iit.edu>
2015-11-17 12:22:09 -0600

    uploading the ACTUAL script!!!

--- install-webserver.sh
+++ install-webserver.sh
@@ -7,7 +7,11 @@ git clone https://github.com/Mahak-Patil/Launch-Setup
 git clone https://github.com/Mahak-Patil/Environment-Setup
 git clone https://github.com/Mahak-Patil/Application-Setup 
 
-mv ./Environment-Setup/images /var/www/html/images
+sudo mv ./Environment-Setup/images /var/www/html/images
+sudo mv ./Application-Setup/*.php /var/www/html
+sudo mv ./Application-Setup/index.html /var/www/html
+sudo mv ./Launch-Setup/*.css /var/www/html
+sudo mv ./Launch-Setup/*.js /var/www/html
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
@@ -16,4 +20,4 @@ sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
-sudo chmod 600 /var/www/html/setup.php
+echo "Hello" > /tmp/hello.txt
