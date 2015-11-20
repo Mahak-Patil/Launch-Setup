bf72d3a4a23d71b96b36b511ffa11a7284a6f0e8
Mahak-Patil <MahakPatil@dhcp141.merunorth100.iit.edu>
2015-11-20 16:49:33 -0600

    pushing sns commands

--- launch.sh
+++ launch.sh
@@ -62,7 +62,7 @@ TopicARN = $(aws sns create-topic --name ITMO-544-Notification)
 echo "The topic ARN is: "$TopicARN
 
 # Setting an attribute of the above topic to a new value
-#aws sns set-topic-attributes --topic-arn $TopicARN --attribute-name DisplayName --attribute-value ITMO-544
+aws sns set-topic-attributes --topic-arn $TopicARN --attribute-name DisplayName --attribute-value ITMO-544
 
 # subscribing an endpoint to a topic
 #aws sns subscribe --topic-arn $TopicARN --protocol sms --notification-endpoint 13123949795
